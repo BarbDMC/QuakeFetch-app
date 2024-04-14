713ce63fd4189f657cd847ce37e569431020f60b
BarbDMC <barbaradmorantesc@gmail.com>
2024-04-14 11:12:36 -0500

    feat: Initialize frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Quake App",
+  description: "Web app to expose seismic data from the USGS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
