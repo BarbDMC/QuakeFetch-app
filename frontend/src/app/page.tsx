c59903a0f447e7aacf28910006af3be313a17537
BarbDMC <barbaradmorantesc@gmail.com>
2024-04-14 11:15:00 -0500

    feat: Add Sidebar component
    Sidebar app component was created

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
-import { Navbar } from "./components/Navbar/Navbar";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
     <main className="">
-      <Navbar />
+      <Sidebar />
     </main>
   );
 }
