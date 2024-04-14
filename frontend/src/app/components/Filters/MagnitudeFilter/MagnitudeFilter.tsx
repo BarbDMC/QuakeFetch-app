01ee9b49c0ff2ebf0e3f3580b622e42454a0bf80
BarbDMC <barbaradmorantesc@gmail.com>
2024-04-14 11:52:00 -0500

    feat: Add App name display component

--- frontend/src/app/components/Filters/MagnitudeFilter/MagnitudeFilter.tsx
+++ frontend/src/app/components/Filters/MagnitudeFilter/MagnitudeFilter.tsx
@@ -4,7 +4,7 @@ import { magnitudes } from "./magnitudes";
 export const MagnitudeFilter = () => {
   return (
     <nav className="h-screen flex flex-col items-center justify-center ">
-      <h1 className="text-black text-3xl font-extrabold font-['Nunito']">Magnitude</h1>
+      <h2 className="text-black text-3xl font-extrabold font-['Nunito']">Magnitude</h2>
       <div className="border-2 border-gray-200 m-4 p-2 rounded-lg w-52 gap-2">
       {
         magnitudes.map((item, index) => (
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
+import { AppNameDisplay } from "./components/AppNameDisplay/AppNameDisplay";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
     <main className="">
+      <AppNameDisplay />
       <Sidebar />
     </main>
   );
